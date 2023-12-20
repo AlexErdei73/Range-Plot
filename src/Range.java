549a52a82c658f152a4582c7f33b9dda74727f97
Alex Erdei <mralexerdei@yahoo.co.uk>
2023-12-20 00:25:06 +0000

    Make two plots in the same window

--- src/Range.java
+++ src/Range.java
@@ -1,18 +1,17 @@
 public class Range {
-    private double speed, angle, range;
+    private static Plot rangePlot = new Plot("Projectile range as function of angle", 0, 90, 5, 0, 50, 10);
+    private double speed;
     Range(double speed) {
         this.speed = speed;
     }
     public double calc(double angleInGrads) {
-        double g, angleInRads;
+        double g, angleInRads, range;
         g = 9.8;
-        this.angle = angleInGrads;
         angleInRads = angleInGrads / 180 * Math.PI;
-        this.range = 2 * this.speed * this.speed / g * Math.sin(angleInRads) * Math.cos(angleInRads);
-        return this.range;
+        range = 2 * this.speed * this.speed / g * Math.sin(angleInRads) * Math.cos(angleInRads);
+        return range;
     }
     public void plot() {
-        Plot rangePlot = new Plot("Projectile range as function of angle", 0, 90, 5, 0, 50, 10);
         for (double angle = 0; angle <= 90; angle++) {
             rangePlot.addPoint(angle, calc(angle));
         }

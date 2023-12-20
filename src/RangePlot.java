cbb47fa3ac2194fcbb3e7d44548d078e000ea79b
Alex Erdei <mralexerdei@yahoo.co.uk>
2023-12-20 00:24:37 +0000

    Make two plots in two different windows

--- src/RangePlot.java
+++ src/RangePlot.java
@@ -5,7 +5,9 @@ the Plot.java class to make the plot.
  */
 public class RangePlot {
     public static void main(String[] args) {
-        Range range = new Range(20);
-        range.plot();
+        Range range20 = new Range(20);
+        range20.plot();
+        Range range10 = new Range(10);
+        range10.plot();
     }
 }
